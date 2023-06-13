de8694b97bc7a61f04ef4318c2d011cef33f56a1
Zurceel <gabrielcruzsil@gmail.com>
2023-06-13 11:32:01 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioAula7/Program.cs
@@ -0,0 +1,50 @@
+﻿string mensagem = "";
+
+try
+{
+    List<string> listamercado = new List<string>()
+    {
+        "Tomate",
+        "Coca-Cola",
+        "Arroz",
+        "Feijão",
+        "Macarrão",
+        "Banana",
+        "Agua",
+        "Chocolate",
+        "Mel"
+    };
+
+    string produtosselecionado;
+
+    Console.WriteLine("Digite o produto que gostaria de comprar:");
+
+    produtosselecionado = Console.ReadLine();
+
+    bool validaproduto = listamercado.Where(l => l.Equals(produtosselecionado)).Any();
+
+    if (validaproduto)
+    {
+        Console.WriteLine("Pode comprar o produto " + produtosselecionado + " pois ele está disponivel");
+    }
+    else
+    {
+        Console.WriteLine("O produto " + produtosselecionado + " não está a venda");
+    }
+
+    Console.WriteLine("Os produtos disponiveis são:");
+    foreach(string produto in listamercado.OrderBy(produto => produto))
+    {
+        Console.WriteLine(produto);
+    }
+
+    mensagem = "Obrigado por ter utilizado o sistema do mercado. Boas compras!";
+
+}catch(Exception e)
+{
+    mensagem = "Aconteceu o seguinte problema: " + e.Message;
+}
+finally
+{
+    Console.WriteLine(mensagem);
+}
